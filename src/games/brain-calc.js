c0ae154e2345666099d7858140b81b81eff73ace
eligoldf <eli.goldfdr@gmail.com>
2019-08-19 20:21:30 +0300

    create brain-calc

---
+++ src/games/brain-calc.js
@@ -0,0 +1,36 @@
+import playGame from '../index';
+import randomNumber from '../functions/randomNumber';
+
+const gameInstruction = 'What is the result of the expression?';
+const playBrainCalc = () => {
+  const randomNumber1 = randomNumber(1, 100);
+  const randomNumber2 = randomNumber(1, 100);
+  const randomOperator = randomNumber(1, 4);
+  let setQuestion;
+  let getRightAnswer;
+  switch (randomOperator) {
+    case 1:
+      setQuestion = `${randomNumber1} + ${randomNumber2}`;
+      getRightAnswer = `${randomNumber1 + randomNumber2}`;
+      break;
+    case 2:
+      setQuestion = `${randomNumber1} - ${randomNumber2}`;
+      getRightAnswer = `${randomNumber1 - randomNumber2}`;
+      break;
+    case 3:
+      setQuestion = `${randomNumber1} * ${randomNumber2}`;
+      getRightAnswer = `${randomNumber1 * randomNumber2}`;
+      break;
+    case 4:
+      setQuestion = `${randomNumber1} / ${randomNumber2}`;
+      getRightAnswer = `${randomNumber1 * randomNumber2}`;
+      break;
+    default:
+      break;
+  }
+  return { setQuestion, getRightAnswer };
+};
+
+export default () => {
+  playGame(gameInstruction, playBrainCalc);
+};

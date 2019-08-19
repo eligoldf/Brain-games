a5ab61ed9d9d8ed14a637a532c43e36ccedf961c
eligoldf <eli.goldfdr@gmail.com>
2019-08-19 20:16:23 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+const playGame = (gameInstruction, startGame) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(gameInstruction);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+
+  const iter = (count, maxCount = 3) => {
+    if (count >= maxCount) {
+      console.log(`Congratulation, ${userName}!`);
+      return 0;
+    }
+    const { setQuestion, getRightAnswer } = startGame();
+    console.log(`Question: ${setQuestion}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (playerAnswer === getRightAnswer) {
+      console.log('Correct!');
+      return iter(count + 1);
+    }
+    console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${getRightAnswer}.
+      Let's try again, ${userName}!`);
+    return iter(count);
+  };
+  return iter(0);
 };
-export default userName;
+export default playGame;

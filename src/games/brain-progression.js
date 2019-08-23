bbec1789856e7c4a4f9ae4e2ca879a0029c0b8a2
eligoldf <eli.goldfdr@gmail.com>
2019-08-23 17:50:57 +0300

    brainProgressionGame update

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,21 +3,20 @@ import makeRandomNumber from '../functions/randomNumber';
 
 const gameInstruction = 'What number is missing in the progression?';
 
-const brainProgressionGame = () => {
-  const makeProgressionNumber = (start, scope, step) => start + (scope * (step - 1));
+const makeProgressionNumber = (start, scope, step) => start + (scope * (step - 1));
 
-  const makeProgression = (start, scope, step) => {
-    let result = '';
-    for (let i = 1; i < 10; i += 1) {
-      if (i === step) {
-        result += ' ..';
-      } else {
-        result += ` ${makeProgressionNumber(start, scope, i)}`;
-      }
+const makeProgression = (start, scope, step) => {
+  let result = '';
+  for (let i = 1; i < 10; i += 1) {
+    if (i === step) {
+      result += ' ..';
+    } else {
+      result += ` ${makeProgressionNumber(start, scope, i)}`;
     }
-    return result;
-  };
-
+  }
+  return result;
+};
+const brainProgressionGame = () => {
   const firstNumber = makeRandomNumber(1, 10);
   const difference = makeRandomNumber(1, 5);
   const hiddenNumber = makeRandomNumber(1, 10);

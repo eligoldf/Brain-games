928ca5eac4ece45ec55608148c111937338d7836
eligoldf <eli.goldfdr@gmail.com>
2019-08-23 17:49:11 +0300

    update isPrime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,18 +1,19 @@
 import playGame from '../index';
 import makeRandomNumber from '../functions/randomNumber';
 
-const gameInstruction = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const gameInstruction = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
 
 const brainPrimeGame = () => {
-  const isPrime = (num) => {
-    if (num < 2) {
-      return false;
-    }
-    for (let i = 2; i < num; i += 1) {
-      if (num % i === 0) return false;
-    }
-    return true;
-  };
 
   const question = makeRandomNumber(1, 100);
   const rightAnswer = isPrime(question) ? 'yes' : 'no';

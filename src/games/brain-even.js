effc56cd234cb7d444066a870cf61e69d64692f1
eligoldf <eli.goldfdr@gmail.com>
2019-08-26 15:07:39 +0300

    small changes in brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,14 +3,12 @@ import makeRandomNumber from '../utils';
 
 const gameInstruction = 'Answer "yes" if number even otherwise answer "no".';
 
-const isEven = (num) => num % 2 === 0;
-
 const minNumber = 1;
 const maxNumber = 100;
 
 const questionAndAnswer = () => {
   const question = makeRandomNumber(minNumber, maxNumber);
-  const rightAnswer = isEven(question) ? 'yes' : 'no';
+  const rightAnswer = question % 2 === 0 ? 'yes' : 'no';
 
   return { question, rightAnswer };
 };

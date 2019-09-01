89025a8aa984af9bfc8e34ecc2aa1d210816e501
eligoldf <eli.goldfdr@gmail.com>
2019-09-01 10:32:52 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const userAttemptsCount = 3;
 
-const startGame = (gameInstruction, questionAndAnswer) => {
+const playGame = (gameInstruction, questionAndAnswer) => {
   console.log('Welcome to the Brain Games!');
   console.log(gameInstruction);
   const userName = readlineSync.question('May I have your name? ');
@@ -29,4 +29,4 @@ const startGame = (gameInstruction, questionAndAnswer) => {
   iter(0);
 };
 
-export default startGame;
+export default playGame;
